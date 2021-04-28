e7b5ba59e42f635aee3e7d75000056bcfd2f9632
Mohamed Hassan <mhass405@gmail.com>
2021-04-28 04:21:35 +0000

    Add additional queries

---
+++ 4_queries/2_assistance_request_data.sql
@@ -0,0 +1,6 @@
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, ( assistance_requests.completed_at - assistance_requests.started_at) as duration
+  FROM assistance_requests
+  JOIN assignments ON assignments.id = assignment_id
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  ORDER BY duration;
